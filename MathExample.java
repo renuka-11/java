15493716e6cb8e3ceb3babc1a5536e3d29d94985
renuka-11 <23pa1a0541@vishnu.edu.in>
2024-10-03 16:39:39 +0530

    Add files via upload

---
+++ MathExample.java
@@ -0,0 +1,8 @@
+public class MathExample {
+      public static void main(String[] args) {
+	int x = -10;
+	double y = -3.14;
+	System.out.println(Math.abs(x));
+	System.out.println(Math.abs(y));
+	}
+     }
