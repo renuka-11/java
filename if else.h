1531962ffb8fd7741e03f454c5e4c1532b5f45a1
renuka-11 <23pa1a0541@vishnu.edu.in>
2025-03-10 20:57:49 +0530

    Create if else.h

---
+++ if else.h
@@ -0,0 +1,27 @@
+
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        int N = scanner.nextInt();
+        if(N%2!=0){
+          System.out.println("Weird");
+        }
+        else{
+            if(N>=2 && N<=5){
+               System.out.println("Not Weird");
+            }
+            else if(N>=6 && N<=20){
+                System.out.println("Weird");
+            }
+            else{
+                System.out.println("Not Weird");
+            }
+        }
+        
+        scanner.close();
+    }
+
+        
+}
